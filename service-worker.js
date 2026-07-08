62f88a7a6fa805eb45a8365f33577032a164ef01
dkathio <kathurima.kdavid.davidjoseph@gmail.com>
2026-07-08 22:38:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'commission-ledger-v1';
+const CACHE_NAME = 'commission-ledger-v2';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
